ec66fbbaf0172e1973bf0be7f8173744d3080add
veyseltosun <postalarim1923@gmail.com>
2022-11-28 13:49:26 +0300

    hamza

---
+++ firecontact/src/components/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQYJt-DOpxngxQZDz0hgokRo1eSBZLuoI",
+    authDomain: "firecontact-crud.firebaseapp.com",
+    projectId: "firecontact-crud",
+    storageBucket: "firecontact-crud.appspot.com",
+    messagingSenderId: "209112045207",
+    appId: "1:209112045207:web:cc77ba5407f7d7fe77bbd2"
+  };
+  
+ 
