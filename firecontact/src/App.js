b9a1dff616ec61184ed6e854dfab02d147b9effa
veyseltosun <postalarim1923@gmail.com>
2022-11-28 10:57:16 +0300

    form handling.

--- firecontact/src/App.js
+++ firecontact/src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Contacts from "./components/contacts/Contacts";
 import FormComponent from "./components/form/Form";
-
+ 
+const initialState = {username:"", phoneNumber:"", gender:"NO INFO"}
 
 function App() {
+  const[info, setInfo]=useState(initialState)
   return (
     <div className="App">
-      <FormComponent/>
+      <FormComponent  info={info} setInfo={setInfo}   />
       <Contacts/>
       
     </div>
--- firecontact/src/components/form/Form.js
+++ firecontact/src/components/form/Form.js
@@ -3,7 +3,23 @@ import {Button, Form, Grid, Segment} from "semantic-ui-react";
 import {options} from "../utils/constant"
 
 
-function FormComponent() {
+
+function FormComponent({info, setInfo }) {
+  const handleSubmit = () =>{
+    console.log("info:", info)
+
+  }
+  const handleInputChange = (e) =>{
+    // const name = e.target.name;
+    // const value = e.targt.value;
+    const{name,value} = e.target;
+    setInfo({...info, [name]:value.toUpperCase()});
+  
+  }
+  const handleOptionChange= (_, result) =>{
+    const {name, value} = result;
+    setInfo({...info, [name]:value.toUpperCase()});
+  }
   return (
     <Grid textAlign='center' verticalAlign="middle">
       <Grid.Column style={{width:"300px"}}>
@@ -12,7 +28,7 @@ function FormComponent() {
             <a href='https://github.com/veyseltosun'
              className='design'
              target="_target"
-            //  rel="noopner noreferrer"
+             rel="noopner noreferrer"
              >
               <code>{"<vys/>"}</code>
               <span className='design header'>design</span>
@@ -20,7 +36,7 @@ function FormComponent() {
            </div>
         </div>
         <h2 className='contact-header'>Add Contact</h2>
-        <Form size='large' onSubmit={() => {}}>
+        <Form size='large' onSubmit={handleSubmit}>
           <Segment stacked>
             <Form.Input
              fluid
@@ -28,9 +44,9 @@ function FormComponent() {
              icon="user"
              iconPosition="left"
              placeholder="Name"
-            //  value = {info.username}
-            // onChange={handleInputcChange}
-            required
+             value = {info.username}
+             onChange={handleInputChange}
+             required
             />
             <Form.Input
               fluid
@@ -39,19 +55,19 @@ function FormComponent() {
               iconPosition="left"
               placeholder="Phone Number"
               type="text"
-            //  value={info.phoneNumber}
-            //  onChange={handleInputChange}
+              value={info.phoneNumber}
+              onChange={handleInputChange}
                required
             />
             <Form.Dropdown
              options={options}
-            // onChange={handleOptionChange}
-            // value={info.gender.toLowerCase()}
-            placeholder='Gender'
-            name="gender"
-            fluid
-            selection
-            required
+             onChange={handleOptionChange}
+             value={info.gender.toLowerCase()}
+             placeholder='Gender'
+             name="gender"
+             fluid
+             selection
+             required
             />
             <Button color='teal' fluid size='large' type='submit'>
               Add
