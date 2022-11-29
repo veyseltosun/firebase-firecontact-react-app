08d0e528b75b91ca249f79dae6c9abcf5252dcc2
veyseltosun <postalarim1923@gmail.com>
2022-11-29 12:19:35 +0300

    delete update

--- firecontact/src/components/contacts/Contacts.js
+++ firecontact/src/components/contacts/Contacts.js
@@ -1,9 +1,56 @@
-import React from 'react'
 
-function Contacts() {
+import { Table, Icon } from "semantic-ui-react";
+import {  useFetch } from "../../utils/functions";
+import { deleteHandler, updateHandler } from "../../utils/functions";
+
+const Contacts = ({updateFormHandler}) => {
+  const { contactList, isLoading } = useFetch();
+
   return (
-    <div>Contacts</div>
-  )
-}
+    <div>
+      <h2 className="contact-header">Contacts</h2>
+      <Table size="large" className="table">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell textAlign="center">Username</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Phone Number</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Gender</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Delete</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Edit</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {isLoading ? (
+            <Table.Row>
+              <Table.Cell colSpan={5} textAlign="center">
+                <p>Loading...</p>
+              </Table.Cell>
+            </Table.Row>
+          ) : contactList?.length === 0 ? (
+            <Table.Row>
+              <Table.Cell colSpan={5} textAlign="center">
+                <p className="nothing-found">Nothing found!</p>
+              </Table.Cell>
+            </Table.Row>
+          ) : (
+            contactList?.map((item) => (
+              <Table.Row key={item?.id}>
+                <Table.Cell>{item.username.toUpperCase()}</Table.Cell>
+                <Table.Cell>{item.phoneNumber}</Table.Cell>
+                <Table.Cell>{item.gender}</Table.Cell>
+                <Table.Cell onClick={() =>deleteHandler(item.id)} >
+                  <Icon name="delete" />
+                </Table.Cell>
+                <Table.Cell >
+                  <Icon name="edit" onClick={()=> updateHandler(item.id)}/>
+                </Table.Cell>
+              </Table.Row>
+            ))
+          )}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+};
 
-export default Contacts
+export default Contacts;
--- firecontact/src/utils/functions.js
+++ firecontact/src/utils/functions.js
@@ -1,8 +1,47 @@
 import firebase from "./firebase";
+import { useState, useEffect } from "react";
+import { successToastify } from "./customToasitfy";
 
 export const addInfo = (info) => {
 
     const contactRef = firebase.database().ref("contact");
     contactRef.push(info)
+    successToastify("Added successfully");
+}
+
+export const useFetch = () => {
+    const [contactList, setContactList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    useEffect(() => {
+      setIsLoading(true);
+      const contactRef = firebase.database().ref("contact");
+      contactRef.on("value", (snapshot) => {
+        const contacts = snapshot.val();
+  
+        const contactArray = [];
+        for (let id in contacts) {
+          contactArray.push({ id, ...contacts[id] });
+        }
+        setContactList(contactArray);
+        setIsLoading(false);
+      });
+    }, []);
+
+    
+    return { contactList, isLoading };
+};
+
+export const deleteHandler = (id) =>{
+    const contactRef = firebase.database().ref("contact").child(id);
+    contactRef.remove();
+    successToastify("Deleted successfully");
+    
+}
+export const updateHandler = (info) =>{
+    const contactRef = firebase.database().ref("contact").child(info.id);
+    contactRef.update(info);
+    successToastify("Updated successfully");
+    
 }
 
