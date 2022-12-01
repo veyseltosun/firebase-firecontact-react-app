6d6c5a9ba64440aec34191441b18195157581893
veyseltosun <postalarim1923@gmail.com>
2022-12-01 08:05:33 +0300

    ENV

--- firecontact/src/utils/firebase.js
+++ firecontact/src/utils/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/database";
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCQYJt-DOpxngxQZDz0hgokRo1eSBZLuoI",
-    authDomain: "firecontact-crud.firebaseapp.com",
-    projectId: "firecontact-crud",
-    storageBucket: "firecontact-crud.appspot.com",
-    messagingSenderId: "209112045207",
-    appId: "1:209112045207:web:cc77ba5407f7d7fe77bbd2"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT_APP_API,
   };
 
   firebase.initializeApp(firebaseConfig);
