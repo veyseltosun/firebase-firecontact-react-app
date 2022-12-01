edbf676177e253bb902e102a8eafd7ef12503d2b
veyseltosun <postalarim1923@gmail.com>
2022-12-01 23:52:32 +0300

    last commit.

--- firecontact/src/utils/firebase.js
+++ firecontact/src/utils/firebase.js
@@ -1,22 +1,19 @@
-import firebase from "firebase/app";
 import "firebase/database";
+import firebase from "firebase/app";
 
-  const devConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId:process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID ,
-    appId: process.env.REACT_APP_API,
-  };
 
-  const prodConfig = {};
-  
-  const firebaseConfig = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
-   
+const firebaseConfig = {
+  apiKey: "AIzaSyCQYJt-DOpxngxQZDz0hgokRo1eSBZLuoI",
+  authDomain: "firecontact-crud.firebaseapp.com",
+  databaseURL: "https://firecontact-crud-default-rtdb.firebaseio.com",
+  projectId: "firecontact-crud",
+  storageBucket: "firecontact-crud.appspot.com",
+  messagingSenderId: "209112045207",
+  appId: "1:209112045207:web:cc77ba5407f7d7fe77bbd2"
+};
 
-  firebase.initializeApp(firebaseConfig);
 
-  export default firebase;
+firebase.initializeApp(firebaseConfig)
+export default firebase;
   
  
